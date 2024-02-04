ad586ff5e7604fa2a0fecf07631647da45cc22dd
Mohammad-Jamal <m.mohammadjamal1961@gmail.com>
2024-02-04 13:00:08 +0530

    Updates

--- src/components/Buttons.js
+++
@@ -1,8 +0,0 @@
-function Buttons({ onClickHandler, value, title }) {
-  return (
-    <button onClick={onClickHandler} className="btns" value={value}>
-      {title}
-    </button>
-  );
-}
-export default Buttons;
